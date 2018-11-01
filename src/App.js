136750b79111eb02a21618e5a123a50a469b46ec
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-11-01 18:08:09 +0300

    Add dunamic content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello!</h1>
+        <Person></Person>
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+  return (
+    <h1>Person I'm { Math.random() }</h1>
+  )
+}
+
+export default person;
